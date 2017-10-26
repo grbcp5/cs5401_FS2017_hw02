075194d56d94bd60458cdd7ca7b12f2cae631a89
grbcp5 <grbcp5@mst.edu>
2017-10-25 22:30:03 -0500

    Create Tree data structure

---
+++ src/edu/mst/grbcp5/hw02/tree/Node.java
@@ -0,0 +1,44 @@
+package edu.mst.grbcp5.hw02.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Node< T > {
+  private T data;
+  private Node< T > parent;
+  private List< Node< T > > children;
+
+  public Node() {
+    this.children = new ArrayList<>();
+  }
+
+  public Node( Node< T > copy ) {
+    this.children = new ArrayList<>( copy.getChildren().size() );
+
+    for ( Node< T > copyChild : copy.getChildren() ) {
+      this.children.add( new Node< T >( copyChild ) );
+    }
+
+  }
+
+  public T getData() {
+    return data;
+  }
+
+  public void setData( T data ) {
+    this.data = data;
+  }
+
+  public Node< T > getParent() {
+    return parent;
+  }
+
+  public void setParent( Node< T > parent ) {
+    this.parent = parent;
+  }
+
+  public List< Node< T > > getChildren() {
+    return children;
+  }
+
+}
