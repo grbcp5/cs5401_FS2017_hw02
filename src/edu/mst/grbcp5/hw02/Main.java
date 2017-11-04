7dc53bc6e62e1caf8403ddb1ef1984c0fb35abab
grbcp5 <grbcp5@mst.edu>
2017-11-04 16:14:21 -0500

    General cleanup

--- src/edu/mst/grbcp5/hw02/Main.java
+++ src/edu/mst/grbcp5/hw02/Main.java
@@ -7,50 +7,46 @@ import edu.mst.grbcp5.hw02.search.Individual;
 import edu.mst.grbcp5.hw02.search.IteratedPrisonerDilemma.PrisonersDilemmaRandomSearch;
 import edu.mst.grbcp5.hw02.search.IteratedPrisonerDilemma.PrisonersDilemmaRandomSearchDelegate;
 import edu.mst.grbcp5.hw02.search.random.RandomSearch;
-import org.xml.sax.SAXException;
 
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.IOException;
 import java.util.Random;
 
 public class Main {
 
   public static final boolean DEBUG = true;
 
-  public static void main( String[] args ) {
+  public static void main( String[] args ) throws Exception {
 
+    /* Local Variables */
     String configFilePath;
-    ConfigFileReader fileReader;
     Parameters parameters;
     PrisonersDilemmaRandomSearchDelegate delegate;
     RandomSearch searcher;
-    Long rndSeed;
+    String searchType;
 
+    /* Check for program arguments */
     if ( args.length != 1 ) {
       System.out.println( "Usage: run.sh <configFilePath>" );
       return;
     }
 
+    /* Get system parameters */
     configFilePath = args[ 0 ];
-    try {
-      fileReader = new ConfigFileReader( configFilePath );
-    } catch ( ParserConfigurationException | IOException | SAXException e ) {
-      e.printStackTrace();
-      return;
-    }
+    parameters = getParameters( configFilePath );
 
-    parameters = new Parameters( fileReader.getParameters() );
+    /* Seed random number generator based on parameters */
+    seedRandomNumberGenerator( parameters );
 
-    if ( parameters.getBoolean( Param.SHOULD_SEED_RANDOM ) ) {
-      rndSeed = parameters.getLong( Param.RANDOM_SEED );
-    } else {
-      rndSeed = System.currentTimeMillis();
+    /* Check search type */
+    searchType = parameters.getString( Param.SEARCH_TYPE );
+    if ( !searchType.toLowerCase().equals( "random" ) ) {
+
+      /* Only support random search */
+      throw new Exception( "Only random search supported" );
+
     }
-    GRandom.setInstance( new Random( rndSeed ) );
 
     delegate = new PrisonersDilemmaRandomSearchDelegate( parameters );
-    searcher = new PrisonersDilemmaRandomSearch( parameters, delegate );
-
+    searcher = new PrisonersDilemmaRandomSearch( delegate );
 
     searcher.getRandomIndividuals( 10000 );
     Individual i = delegate.getCurrentBest();
@@ -58,6 +54,36 @@ public class Main {
     System.out.println( "Best individual: " );
     System.out.println( "\tPreorder: " + i );
     System.out.println( "\tFitness: " + i.getFitness() );
+
+  }
+
+  private static Parameters getParameters( String configFilePath ) throws
+    Exception {
+
+    /* Local Variables */
+    ConfigFileReader fileReader;
+    Parameters parameters;
+
+    fileReader = new ConfigFileReader( configFilePath );
+
+
+    parameters = new Parameters( fileReader.getParameters() );
+
+
+    return parameters;
+  }
+
+  private static void seedRandomNumberGenerator( Parameters parameters ) {
+
+    Long rndSeed;
+
+    if ( parameters.getBoolean( Param.SHOULD_SEED_RANDOM ) ) {
+      rndSeed = parameters.getLong( Param.RANDOM_SEED );
+    } else {
+      rndSeed = System.currentTimeMillis();
+    }
+
+    GRandom.setInstance( new Random( rndSeed ) );
   }
 
 }
--- src/edu/mst/grbcp5/test/search/random/PrisonersDilemmaRandomSearchTest.java
+++ src/edu/mst/grbcp5/test/search/random/PrisonersDilemmaRandomSearchTest.java
@@ -1,115 +1,115 @@
-package edu.mst.grbcp5.test.search.random;
-
-import edu.mst.grbcp5.hw02.GRandom;
-import edu.mst.grbcp5.hw02.input.Param;
-import edu.mst.grbcp5.hw02.input.Parameters;
-import edu.mst.grbcp5.hw02.search.IteratedPrisonerDilemma.*;
-import edu.mst.grbcp5.hw02.tree.Node;
-import edu.mst.grbcp5.hw02.tree.Tree;
-import org.junit.Before;
-import org.junit.Test;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
-public class PrisonersDilemmaRandomSearchTest {
-
-  public static final int NUM_TEST_CASES = 10000;
-
-  private PrisonersDilemmaRandomSearch fut; // file under test
-  private Parameters parameters;
-
-  @Before
-  public void setUp() throws Exception {
-
-    GRandom.setInstance( new Random( 10021996 ) );
-
-    Map< String, Object > p;
-
-    p = new HashMap<>();
-    p.put( Param.MAX_TREE_DEPTH.identifier(), 10 );
-    p.put( Param.MEMORY_DEPTH.identifier(), 5 );
-    p.put( Param.DEPTH_PROPORTIONAL_INIT.identifier(), false );
-    this.parameters = new Parameters( p );
-    this.fut = new PrisonersDilemmaRandomSearch( parameters, null );
-
-  }
-
-  @Test
-  public void getRandomIndividual() throws Exception {
-
-    Prisoner p;
-    Tree< StrategyFunction > t;
-    Node< StrategyFunction > n;
-    int maxTreeDepth;
-    int height;
-    int heights[];
-
-    maxTreeDepth = this.parameters.getInteger( Param.MAX_TREE_DEPTH );
-    heights = new int[ maxTreeDepth + 1 ];
-
-    for ( int i = 0; i < NUM_TEST_CASES; i++ ) {
-      p = ( Prisoner ) fut.getRandomIndividual();
-      t = p.getStrategy();
-      n = t.getRoot();
-
-      height = t.getHeight();
-      assertTrue( height <= maxTreeDepth );
-      this.testNode( n );
-      System.out.println( "Tree passed: " + t );
-
-      heights[ height ]++;
-
-    }
-
-    System.out.println( "\nHeights: " );
-    for ( int i = 0; i < maxTreeDepth + 1; i++ ) {
-      System.out.println( "Height " + i + ": " + heights[ i ] +
-                            " (" +
-                            ( ( double ) heights[ i ] ) / NUM_TEST_CASES +
-                            ")" );
-    }
-
-  }
-
-  private void testNode( Node<StrategyFunction> n ) throws Exception {
-
-    /* Local variables */
-    Terminal terminal;
-    LogicalOperator op;
-
-    /* Test node depending on type */
-    switch ( n.getData().getType() ) {
-      case LOGICAL_OPERATOR:
-        op = ( LogicalOperator ) n.getData();
-
-        /* Assert node has correct number of children */
-        if( op.getOperator() == Operator.NOT ) {
-          assertEquals( 1, n.getChildren().size() );
-        } else {
-          assertEquals( 2, n.getChildren().size() );
-        }
-
-        /* Test each child node */
-        for ( Node< StrategyFunction > child : n.getChildren() ) {
-          testNode( child );
-        }
-
-        break;
-      case TERMINAL:
-        terminal = ( Terminal ) n.getData();
-
-        /* Assert memory reference isn't greater than memory depth */
-        assertTrue( terminal.getTime()
-                      < this.parameters.getInteger( Param.MEMORY_DEPTH ) );
-        assertTrue( n.getChildren().isEmpty() );
-        break;
-    }
-
-  }
-
-}
+//package edu.mst.grbcp5.test.search.random;
+//
+//import edu.mst.grbcp5.hw02.GRandom;
+//import edu.mst.grbcp5.hw02.input.Param;
+//import edu.mst.grbcp5.hw02.input.Parameters;
+//import edu.mst.grbcp5.hw02.search.IteratedPrisonerDilemma.*;
+//import edu.mst.grbcp5.hw02.tree.Node;
+//import edu.mst.grbcp5.hw02.tree.Tree;
+//import org.junit.Before;
+//import org.junit.Test;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//import java.util.Random;
+//
+//import static org.junit.Assert.assertEquals;
+//import static org.junit.Assert.assertTrue;
+//
+//public class PrisonersDilemmaRandomSearchTest {
+//
+//  public static final int NUM_TEST_CASES = 10000;
+//
+//  private PrisonersDilemmaRandomSearch fut; // file under test
+//  private Parameters parameters;
+//
+//  @Before
+//  public void setUp() throws Exception {
+//
+//    GRandom.setInstance( new Random( 10021996 ) );
+//
+//    Map< String, Object > p;
+//
+//    p = new HashMap<>();
+//    p.put( Param.MAX_TREE_DEPTH.identifier(), 10 );
+//    p.put( Param.MEMORY_DEPTH.identifier(), 5 );
+//    p.put( Param.DEPTH_PROPORTIONAL_INIT.identifier(), false );
+//    this.parameters = new Parameters( p );
+//    this.fut = new PrisonersDilemmaRandomSearch( parameters, null );
+//
+//  }
+//
+//  @Test
+//  public void getRandomIndividual() throws Exception {
+//
+//    Prisoner p;
+//    Tree< StrategyFunction > t;
+//    Node< StrategyFunction > n;
+//    int maxTreeDepth;
+//    int height;
+//    int heights[];
+//
+//    maxTreeDepth = this.parameters.getInteger( Param.MAX_TREE_DEPTH );
+//    heights = new int[ maxTreeDepth + 1 ];
+//
+//    for ( int i = 0; i < NUM_TEST_CASES; i++ ) {
+//      p = ( Prisoner ) fut.getRandomIndividual();
+//      t = p.getStrategy();
+//      n = t.getRoot();
+//
+//      height = t.getHeight();
+//      assertTrue( height <= maxTreeDepth );
+//      this.testNode( n );
+//      System.out.println( "Tree passed: " + t );
+//
+//      heights[ height ]++;
+//
+//    }
+//
+//    System.out.println( "\nHeights: " );
+//    for ( int i = 0; i < maxTreeDepth + 1; i++ ) {
+//      System.out.println( "Height " + i + ": " + heights[ i ] +
+//                            " (" +
+//                            ( ( double ) heights[ i ] ) / NUM_TEST_CASES +
+//                            ")" );
+//    }
+//
+//  }
+//
+//  private void testNode( Node<StrategyFunction> n ) throws Exception {
+//
+//    /* Local variables */
+//    Terminal terminal;
+//    LogicalOperator op;
+//
+//    /* Test node depending on type */
+//    switch ( n.getData().getType() ) {
+//      case LOGICAL_OPERATOR:
+//        op = ( LogicalOperator ) n.getData();
+//
+//        /* Assert node has correct number of children */
+//        if( op.getOperator() == Operator.NOT ) {
+//          assertEquals( 1, n.getChildren().size() );
+//        } else {
+//          assertEquals( 2, n.getChildren().size() );
+//        }
+//
+//        /* Test each child node */
+//        for ( Node< StrategyFunction > child : n.getChildren() ) {
+//          testNode( child );
+//        }
+//
+//        break;
+//      case TERMINAL:
+//        terminal = ( Terminal ) n.getData();
+//
+//        /* Assert memory reference isn't greater than memory depth */
+//        assertTrue( terminal.getTime()
+//                      < this.parameters.getInteger( Param.MEMORY_DEPTH ) );
+//        assertTrue( n.getChildren().isEmpty() );
+//        break;
+//    }
+//
+//  }
+//
+//}
