924adfbecbfe7035a644a653a66a68b1df415405
grbcp5 <grbcp5@mst.edu>
2017-11-19 21:33:39 -0600

    Work on report

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@
 mkdir log
 mkdir sol
 
-mkdir log/2a
-mkdir sol/2a
+mkdir log/2b
+mkdir sol/2b
 
 javac  -verbose $( find ./src/* -not -name "*Test.java" | grep .java ) -d out/
 java -cp out edu.mst.grbcp5.hw02.Main $1 $2
