f26a27329c2008870b590ef8d6b5a9b1cfeb806b
grbcp5 <grbcp5@mst.edu>
2017-12-03 19:19:17 -0600

    Update run.sh script

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-mkdir log
-mkdir sol
+mkdir -p log
+mkdir -p sol
 
-mkdir log/2b
-mkdir sol/2b
+mkdir -p log/2c
+mkdir -p sol/2c
 
-javac  -verbose $( find ./src/* -not -name "*Test.java" | grep .java ) -d out/
+javac -verbose $( find ./src/* -not -name "*Test.java" | grep .java ) -d out/
 java -cp out edu.mst.grbcp5.hw02.Main $1 $2
